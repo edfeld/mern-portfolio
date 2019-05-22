ee691d38351f72ff4cd55d4579d591f1a29b927d
edfeld <edfeld1@gmail.com>
2019-05-22 14:36:51 -0700

    some package vers chgs, cosmetic chg in NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ function NavBar () {
           <button className="navbar-toggler col-2" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse col-md-3 offset-md-4 " id="navbarCollapse">
+          <div className="collapse navbar-collapse col-md-3 offset-md-4" id="navbarCollapse">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
                 <Link className={window.location.pathname === "/" ? "nav-link active" : "nav-link"} to="/">About</Link>
